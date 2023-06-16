d4b5e9d6caf6b68960087291d9e63f57f6500e17
Yana G <ivana-1@mail.ru>
2023-06-16 12:57:28 +0300

    SavingAccountTestAdded

--- src/main/java/ru/netology/javaqadiplom/SavingAccount.java
+++ src/main/java/ru/netology/javaqadiplom/SavingAccount.java
@@ -51,6 +51,7 @@ public class SavingAccount extends Account {
         } else {
             return false;
         }
+
     }
 
     /**
--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -6,7 +6,28 @@ import org.junit.jupiter.api.Test;
 public class SavingAccountTest {
 
     @Test
-    public void shouldAddLessThanMaxBalance() {
+    public void shouldThrowExceptionIfMinBalanceGreaterMaxBalance() { //должно вызываться исключение, если минимальный баланс больше максимального баланса
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(2_000, 11_000, 10_000, 5);
+        });
+    }
+
+    @Test
+    public void shouldThrowExceptionIfRateIsNegative() { //должен выдать исключение, если ставка отрицательна
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(2_000, 1_000, 10_000,-5);
+        });
+    }
+
+    @Test
+    public void shouldThrowExceptionIfBalanceIsNegative() { //должно вызывать исключение, если баланс отрицательный
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(-2000, 1_000, 10_000,5);
+        });
+    }
+
+    @Test
+    public void whenAfterPurchaseBalanceIsNegative() { //когда баланс после покупки отрицательный
         SavingAccount account = new SavingAccount(
                 2_000,
                 1_000,
@@ -14,8 +35,156 @@ public class SavingAccountTest {
                 5
         );
 
-        account.add(3_000);
+        account.pay(3_000);
 
-        Assertions.assertEquals(2_000 + 3_000, account.getBalance());
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test
+    public void whenAfterPurchaseBalanceIsLessThanMinBalance() { //когда баланс после покупки меньше минимального баланса
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.pay(2_000);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test
+    public void whenAfterPurchaseBalanceMoreThanMinBalance() { //когда баланс после покупки превышает минимальный баланс
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.pay(500);
+
+        Assertions.assertEquals(1_500, account.getBalance());
+    }
+
+    @Test
+    public void whenAfterPurchaseBalanceEqualToMinBalance() { //когда баланс после покупки равен минимальному балансу
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.pay(1_000);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test
+    public void whenAmountEqualToZero() { //когда сумма покупки равна нулю
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.pay(0);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test
+    public void whenAfterAddBalanceIsLessThanMaxBalance() { //когда баланс после добавления меньше максимального баланса
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(7_000);
+
+        Assertions.assertEquals(9_000, account.getBalance());
+    }
+
+    @Test
+    public void whenAfterAddBalanceIsMoreThanMaxBalance() { //когда баланс после добавления превышает максимальный баланс
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(9_000);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test
+    public void whenAfterAddBalanceEqualMaxBalance() { //когда после добавления баланс равен максимальному балансу
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(8_000);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldAddZero() { //следует добавить ноль
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(0);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldCalculateAmountPercent() { //следует рассчитать сумму процентов
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        Assertions.assertEquals(100, account.yearChange());
+    }
+
+    @Test
+    public void ifMinBalanceEqualZero() {
+        SavingAccount account = new SavingAccount(
+                0,
+                0,
+                10_000,
+                5
+        );
+
+        Assertions.assertEquals(0, account.getMinBalance());
+    }
+
+    @Test
+    public void ifMaxBalanceEqualZero() {
+        SavingAccount account = new SavingAccount(
+                0,
+                0,
+                0,
+                5
+        );
+
+        Assertions.assertEquals(0, account.getMaxBalance());
     }
 }
